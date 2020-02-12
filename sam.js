222e050636589cf06bdae8308e1086aa48db8986
anjuniranjana <60912419+anjuniranjana@users.noreply.github.com>
2020-02-12 09:14:49 +0530

    Add files via upload

--- sam.js
+++ sam.js
@@ -1,1 +1,10 @@
-#anjuniranjana
+
+var sample1= [{Name: "Anju",class: 1,Age: 5, display: function(){log("Name"+"Class"+"Age")}},
+{Name: "Anjana",class: 2,Age: 6,display:function(){log("Name"+"Class"+"Age")}},
+{Name: "Vaidu",class: 3,Age: 7, display:function(){log("Name"+"Class"+"Age")}},
+{Name: "Lakshmi",class: 4,Age: 8, display:function(){log("Name"+"Class"+"Age")}}]
+
+for(i=0;i<5;i++)
+{
+ log(sample1[i].name);
+ }
